3256f7d9121f1f071e4734c05c731e6bad133fef
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2024-12-09 15:44:44 -0500

    .

--- face-api-js-starter-main/public/glitch_script.js
+++ face-api-js-starter-main/public/glitch_script.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function init() {
         scene = new THREE.Scene();
+        
 
-        //Set up the camera
         camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.set(0, 50, 150);
 
